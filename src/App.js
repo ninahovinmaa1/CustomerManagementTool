4d6e947e6207cf233bee984a8700580bc6dcbb0f
Nina <>
2021-01-13 10:36:46 +0100

    post credentials

--- src/App.js
+++ src/App.js
@@ -14,15 +14,31 @@ function App() {
     const inputValue = e.target.value // users input to email or password
     const newObj = {...formData, [inputName]: inputValue} // spread op to not override object formData. Think [inputName]: inputValue as [email]: nina@nackademin.se
     setFormData(newObj)
+  }
 
-    setFormData({...formData, [e.target.name]: e.target.value})
+  function handleOnSubmit(e) {
+    e.preventDefault()  // prevents reloading site when button login is clicked
+    const url="https://frebi.willandskill.eu/api-token-auth/"
+    const payload = {
+      email: formData.email,
+      password: formData.password
+    }
+    fetch(url, {
+      body: JSON.stringify(payload), //API expects a string, that's why we stringify payload-obj
+      method: "POST",
+      headers: {
+        "Content-Type":"application/json"
+      }
+    })
+    .then(res => res.json())
+    .then(data => console.log(data))
   }
 
   return (
     <div className="container">
       <h1>Customer Relationship Management system CRMI/O </h1>
       <div>
-        <form>
+        <form onSubmit={handleOnSubmit}> 
           <label>Email:</label>
           <input name="email" value={formData.email} onChange={handleOnChange}/>
           <label>Password:</label>
