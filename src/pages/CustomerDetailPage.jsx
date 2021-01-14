8b16770b9edf889a8949b9df7b124bfccc84ddd3
Nina <>
2021-01-14 16:33:16 +0100

    changed customer details to be displayed in a table

--- src/pages/CustomerDetailPage.jsx
+++ src/pages/CustomerDetailPage.jsx
@@ -21,27 +21,65 @@ export default function CustomerDetailPage(props) {
 
     return (
         <div>
-            <h1>Customer Detail Page</h1>
             {customerItem
                 ? (
                     <div>
                         <h1>{customerItem.name}</h1>
-                        <p>{customerItem.organisationNr}</p>
-                        <p>{customerItem.paymentTerm}</p>
-                        <p>{customerItem.phoneNumber}</p>
-                        <p>{customerItem.reference}</p>
-                        <p>{customerItem.vatNr}</p>
-                        <p>{customerItem.email}</p>
-                        <p>{customerItem.website}</p>
+                        <table>
+                            <tbody>
+                                <tr>
+                                    <td>Organisation Number</td>
+                                    <td>{customerItem.organisationNr}</td>
+                                </tr>
 
+                                <tr>
+                                    <td>Payment Term</td>
+                                    <td>{customerItem.paymentTerm}</td>
+                                </tr>
 
+                                <tr>
+                                    <td>Phone Number</td>
+                                    <td>{customerItem.phoneNumber}</td>
+                                </tr>
 
+                                <tr>
+                                    <td>Reference</td>
+                                    <td>{customerItem.reference}</td>
+                                </tr>
+
+                                <tr>
+                                    <td>VAT Number</td>
+                                    <td>{customerItem.vatNr}</td>
+                                </tr>
+
+                                <tr>
+                                    <td>Email</td>
+                                    <td>
+                                        <a href={`mailto:${customerItem.email}`}>
+                                            {customerItem.email}
+                                        </a>
+                                    </td>
+                                </tr>
+
+                                <tr>
+                                    <td>Website</td>
+                                    <td>
+                                        <a href={customerItem.website} target="_blank">
+                                            {customerItem.website}
+                                        </a>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <button onClick={deleteCustomer}>Delete Customer</button>
+                        <Link to={`/customers/${customerId}/edit`}>Edit Customer</Link>
                     </div>
                 )
                 :
                 (
                     <span>Laddar data...</span>
-                )}
+                )
+            }
         </div>
     )
 }
