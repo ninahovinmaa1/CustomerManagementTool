969230ddea794d020cdc56b02d52e6154787118b
Nina <>
2021-01-17 17:56:39 +0100

    modify getting userinfo using context

--- src/App.js
+++ src/App.js
@@ -5,13 +5,16 @@ import CustomerDetailPage from './pages/CustomerDetailPage';
 import CustomerListPage from './pages/CustomerListPage';
 import CustomerUpdatePage from './pages/CustomerUpdatePage';
 import LoginPage from './pages/LoginPage';
+import { CustomerListContext } from './contexts/CustomerListContext';
+import { UserContext } from './contexts/UserContext';
 
 function App() {
 
   const [userData, setUserData] = useState(null);
+  const [customerList, setCustomerList] = useState(null);
   
-  //Get info of which user is logged in and display users email, firstname and lastname.
-  function getMe() {
+  //Get info of which user is logged in
+  function fetchUserData() {
     const url = "https://frebi.willandskill.eu/api/v1/me/"
     const token = localStorage.getItem("WEBB20")
     fetch(url, {
@@ -24,42 +27,60 @@ function App() {
     .then(data => setUserData(data))
   }
 
+  //get available list of customers
+  function getCustomerList() {
+    const url = "https://frebi.willandskill.eu/api/v1/customers/"
+    const token = localStorage.getItem("WEBB20")
+    fetch(url, {
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${token}`
+      }
+    })
+    .then(res => res.json())
+    .then(data => {
+      setCustomerList(data.results)
+    })
+  }
   return (
     <div>
+      <UserContext.Provider value={{userData, setUserData, fetchUserData}}>
+      <CustomerListContext.Provider value={customerList, setCustomerList, getCustomerList}>
+        <ul>
+          <li><Link to="/home">Customers</Link></li>
+          <li><Link to="/customers/create">Create Customer</Link></li>
+          <li><Link to="/login">Login</Link></li>
+        </ul>
 
-      <ul>
-        <li><Link to="/home">Customers</Link></li>
-        <li><Link to="/customers/create">Create Customer</Link></li>
-        <li><Link to="/login">Login</Link></li>
-      </ul>
+        <Switch>
+          <Route path="/login">
+            <LoginPage/>
+          </Route>
 
-      <Switch>
-        <Route path="/login">
-          <LoginPage/>
-        </Route>
+          <Route path="/customers/create">
+            <CustomerCreatePage/>
+          </Route>
 
-        <Route path="/customers/create">
-          <CustomerCreatePage/>
-        </Route>
+          <Route path="/customers/:id/edit" component={CustomerUpdatePage}></Route>
 
-        <Route path="/customers/:id/edit" component={CustomerUpdatePage}></Route>
+          <Route path="/customers/:id" component={CustomerDetailPage}/>
 
-        <Route path="/customers/:id" component={CustomerDetailPage}/>
+          <Route path="/home">
+            <CustomerListPage/>
+          </Route>
 
-        <Route path="/home">
-          <CustomerListPage/>
-        </Route>
+        </Switch>
 
-      </Switch>
+        <h1>Customer Relationship Management system CRMI/O </h1>
+        <div>
+          
+          <hr/>
+          <button onClick={fetchUserData}>Display user</button>
+          {userData && <p> Logged in as: {userData.firstName} {userData.lastName}, {userData.email}</p>}
 
-      <h1>Customer Relationship Management system CRMI/O </h1>
-      <div>
-        
-        <hr/>
-        <button onClick={getMe}>Get Me</button>
-        {userData && <p> Logged in as: {userData.firstName} {userData.lastName}, {userData.email}</p>}
-
-      </div>
+        </div>
+      </CustomerListContext.Provider>
+      </UserContext.Provider>
     </div>
   );
 }
--- src/pages/CustomerCreatePage.jsx
+++ src/pages/CustomerCreatePage.jsx
@@ -22,6 +22,7 @@ export default function CustomerCreatePage() {
                     type={type || "text"}
                     name={name}
                     onChange={handleOnChange}
+                    required
                 />
             </div>
         )
